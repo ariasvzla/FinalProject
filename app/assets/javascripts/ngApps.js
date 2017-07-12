ce4929c9d23c3104b3df92a954abdad2f9883675
Elianny Arias <arias.vzla@gmail.com>
2017-07-12 22:25:44 +0100

    none

--- app/assets/javascripts/ngApps.js
+++ app/assets/javascripts/ngApps.js
@@ -8,57 +8,118 @@ $scope.validation = function (startDate, endDate){
 
 
 if ($("#origin").val()=="") {
-   $("#errorHand").show(300 );
-     $("#errorHand").html("Type an Origin");
-      $("#errorHand").hide(1121);
+ 
+  $(".errorHand").html("Type an Origin");
+   $(".errorHand").addClass('animated jackInTheBox');
+    
+     // $("#errorHand").hide(1121);
      return false;
 }
 if ($("#dest").val()=="") {
-   $("#errorHand").show(300 );
-     $("#errorHand").html("Type a Destination");
-      $("#errorHand").hide(1121);
+
+   $(".errorHand").html("Type a Destination");
+   $(".errorHand").addClass('animated jackInTheBox');
+
+  
+     // $("#errorHand").hide(1121);
      return false;
 }
 if ($("#exampleInputAmount").val()=="") {
-   $("#errorHand").show(300 );
-     $("#errorHand").html("Set a Budget please");
-      $("#errorHand").hide(1121);
+      $("#errorHand").hide();
+       $("#errorHand4").hide();
+     $("#errorHand5").hide();
+       $("#errorHand6").hide();
+     $("#errorHand7").hide();
+  $("#errorHand8").hide();
+  $("#errorHand2").hide();
+
+   $("#errorHand3").addClass('animated jackInTheBox');
+     $("#errorHand3").html("Set a Budget please");
+     // $("#errorHand").hide(1121);
      return false;
   }
-    else if ($("#exampleInputAmount").val()<200){
-      $("#errorHand").show(300 );
-      $("#errorHand").html("Budget too low!");
-      $("#errorHand").hide(1121);
+  if ($("#exampleInputAmount").val()<200){
+          $("#errorHand").hide();
+     $("#errorHand3").hide();
+      $("#errorHand2").hide();
+     $("#errorHand5").hide();
+       $("#errorHand6").hide();
+     $("#errorHand7").hide();
+  $("#errorHand8").hide();
+ 
+       $("#errorHand4").addClass('animated jackInTheBox');
+      $("#errorHand4").html("Budget too low!");
+     
          return false;
       }
+      var d1= startDate;
+      var d2= endDate;
    var nigths = d1-d2;
   
 if (endDate == undefined && startDate==undefined){
-  $("#errorHand").show(300 );
-    $("#errorHand").html("Dates cannot be blank");
-     $("#errorHand").hide(3121);
+   
+         $("#errorHand").hide();
+     $("#errorHand3").hide();
+      $("#errorHand2").hide();
+    
+       $("#errorHand6").hide();
+     $("#errorHand7").hide();
+  $("#errorHand8").hide();
+      $("#errorHand4").hide();
+    $("#errorHand5").addClass('animated jackInTheBox');
+    $("#errorHand5").html("Dates cannot be blank");
+   
     return false;
   }if (startDate==undefined){
-    $("#errorHand").show(300 );
-    $("#errorHand").html("Select a Departure Date please");
-     $("#errorHand").hide(3121);
+          $("#errorHand").hide();
+     $("#errorHand3").hide();
+      $("#errorHand2").hide();
+     $("#errorHand5").hide();
+     $("#errorHand7").hide();
+  $("#errorHand8").hide();
+      $("#errorHand4").hide();
+  
+     $("#errorHand6").addClass('animated jackInTheBox');
+    $("#errorHand6").html("Select a Departure Date please");
+    
     return false;
   }
   if (endDate==undefined){
-    $("#errorHand").show(300 );
-   $("#errorHand").html("Select a Returning Date please");
-    $("#errorHand").hide(3121);
+          $("#errorHand").hide();
+     $("#errorHand3").hide();
+      $("#errorHand2").hide();
+  $("#errorHand8").hide();
+      $("#errorHand4").hide();
+   $("#errorHand6").hide();
+    $("#errorHand5").hide();
+    $("#errorHand7").addClass('animated jackInTheBox');
+   $("#errorHand7").html("Select a Returning Date please");
+    //$("#errorHand").hide(3121);
    return false;
   } if (d1>d2){
-    $("#errorHand").show(300 );
-     $("#errorHand").html("Start date must be before the End date");
-      $("#errorHand").hide(3121);
+         $("#errorHand").hide();
+     $("#errorHand3").hide();
+      $("#errorHand2").hide();
+      $("#errorHand4").hide();
+   $("#errorHand6").hide();
+    $("#errorHand7").hide();
+     $("#errorHand5").hide();
+     $("#errorHand8").addClass('animated jackInTheBox');
+     $("#errorHand8").html("Start date must be before the End date");
+      //$("#errorHand").hide(3121);
        return false;
   }
 
 
   else {
-     
+     $("#errorHand").hide();
+     $("#errorHand2").hide();
+      $("#errorHand3").hide();
+     $("#errorHand4").hide();
+      $("#errorHand5").hide();
+     $("#errorHand6").hide();
+      $("#errorHand7").hide();
+     $("#errorHand8").hide();
     return true;
   }
  }
