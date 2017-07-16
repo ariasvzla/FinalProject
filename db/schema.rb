d7d787fdd9231b96b89073e308d7018d676566f1
Elianny Arias <arias.vzla@gmail.com>
2017-07-16 22:46:45 +0100

    rooms ref to hotelss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716214411) do
+ActiveRecord::Schema.define(version: 20170716214613) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
